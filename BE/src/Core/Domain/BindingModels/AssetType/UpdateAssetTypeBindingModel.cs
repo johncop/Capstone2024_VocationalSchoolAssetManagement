1d6223906dae059a8b5cc11ef005804872e17cdd
Trương Hoàng Long <thoanglong94@gmail.com>
2024-11-18 11:53:20 +0700

    Update Controller and Binding Model: - Update AssetType Binding model - Update Asset Type Controller

--- BE/src/Core/Domain/BindingModels/AssetType/UpdateAssetTypeBindingModel.cs
+++ BE/src/Core/Domain/BindingModels/AssetType/UpdateAssetTypeBindingModel.cs
@@ -3,8 +3,8 @@
     public class UpdateAssetTypeBindingModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public int Quantity { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int? Quantity { get; set; }
     }
 }
